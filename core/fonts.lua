4c2594bb3eba48985572db979fcc41cb7856bf1c
Yago Riveiro <yago.riveiro@gmail.com>
2024-06-10 12:06:28 +0100

    fix: use wezterm again instead w
    Signed-off-by: Yago Riveiro <yago.riveiro@gmail.com>

--- core/fonts.lua
+++ core/fonts.lua
@@ -1,16 +1,16 @@
-local w = require 'wezterm'
+local wezterm = require 'wezterm'
 
 local M = {}
 
 function M.setup(config)
   -- Terminal Fonts
-  config.font = w.font 'MyFont Nerd Font Mono'
+  config.font = wezterm.font 'MyFont Nerd Font Mono'
   config.font_size = 14
   config.bold_brightens_ansi_colors = 'BrightAndBold'
 
   -- Tab Fonts
   config.window_frame = {
-    font = w.font 'MyFont Nerd Font Mono',
+    font = wezterm.font 'MyFont Nerd Font Mon',
     font_size = 14,
   }
 end
