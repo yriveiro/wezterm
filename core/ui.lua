0a251a3fa55fbea8e59477f51711f322cb001eba
Yago Riveiro <yago.riveiro@farfetch.com>
2024-06-21 21:23:18 +0100

    feat: remove window frame
    Signed-off-by: Yago Riveiro <yago.riveiro@farfetch.com>

--- core/ui.lua
+++ core/ui.lua
@@ -4,6 +4,7 @@ function M.setup(config)
   config.front_end = 'WebGpu'
   config.webgpu_power_preference = 'HighPerformance'
   config.audible_bell = 'Disabled'
+  config.window_decorations = 'RESIZE'
 
   config.color_scheme = 'onenord'
   config.scrollback_lines = 10000
