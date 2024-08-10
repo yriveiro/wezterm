509b014efb5336f5f9e8f7ed7f4c445aea0ead9a
Yago Riveiro <yago.riveiro@gmail.com>
2024-08-10 14:47:39 +0100

    feat: add tabs configuration through plugin
    Signed-off-by: Yago Riveiro <yago.riveiro@gmail.com>

--- wezterm.lua
+++ wezterm.lua
@@ -9,12 +9,14 @@ require('config.keys').setup(config)
 require('config.fonts').setup(config)
 require('config.gpu').setup(config)
 require('config.ui').setup(config)
-require('config.tabs').setup(config)
+-- require('config.tabs').setup(config)
 
 wezterm.plugin
   .require('https://github.com/yriveiro/wezterm-status')
   .apply_to_config(config, { cells = { date = {
     format = '%H:%M',
   } } })
+
+wezterm.plugin.require('https://github.com/yriveiro/wezterm-tabs').apply_to_config(config)
 
 return config
