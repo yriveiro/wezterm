8584dca47de71c77379a850bc2f5237f0c6eb7e3
Yago Riveiro <yago.riveiro@farfetch.com>
2024-08-10 17:43:26 +0100

    feat: add resize mode to wezterm-status config
    Signed-off-by: Yago Riveiro <yago.riveiro@farfetch.com>

--- wezterm.lua
+++ wezterm.lua
@@ -5,6 +5,8 @@ local config = wezterm.config_builder()
 
 wezterm.log_info 'Reloading Wezterm configuration'
 
+require('config.key_tables.copy').setup(config)
+require('config.key_tables.resize').setup(config)
 require('config.keys').setup(config)
 require('config.fonts').setup(config)
 require('config.gpu').setup(config)
@@ -13,9 +15,18 @@ require('config.ui').setup(config)
 
 wezterm.plugin
   .require('https://github.com/yriveiro/wezterm-status')
-  .apply_to_config(config, { cells = { date = {
-    format = '%H:%M',
-  } } })
+  .apply_to_config(config, {
+    cells = {
+      date = {
+        format = '%H:%M',
+      },
+      mode = {
+        modes = {
+          resize_mode = ' ' .. wezterm.nerdfonts.md_resize,
+        },
+      },
+    },
+  })
 
 wezterm.plugin.require('https://github.com/yriveiro/wezterm-tabs').apply_to_config(config)
 
