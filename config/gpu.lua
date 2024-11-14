6e8a2183d4e69c0ed4848b13ffed776e8cfb1d3c
Yago Riveiro <yago.riveiro@farfetch.com>
2024-11-14 10:59:49 +0000

    feat: increase fps
    Signed-off-by: Yago Riveiro <yago.riveiro@farfetch.com>

--- config/gpu.lua
+++ config/gpu.lua
@@ -4,7 +4,7 @@ function M.setup(config)
   config.front_end = 'WebGpu'
   config.webgpu_power_preference = 'HighPerformance'
   config.webgpu_force_fallback_adapter = false
-  config.animation_fps = 60
+  config.animation_fps = 120
 end
 
 return M
