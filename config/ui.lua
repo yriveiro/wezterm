05f74432c662ed7e1513e087b0fd9e84259cd8cd
Yago Riveiro <yago.riveiro@gmail.com>
2025-02-12 21:36:56 +0000

    chore: use xterm-kitty by default
    Signed-off-by: Yago Riveiro <yago.riveiro@gmail.com>

--- config/ui.lua
+++ config/ui.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup(config)
+  config.term = 'xterm-kitty'
   config.audible_bell = 'Disabled'
   config.window_decorations = 'RESIZE'
   config.enable_kitty_graphics = true
